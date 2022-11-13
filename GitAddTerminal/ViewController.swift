571909ff7b571b677e486293dc7f2fd8a1b02601
NiyozbekYuldashev <yniyozbek@gmail.com>
2022-11-13 11:50:31 +0500

    setUpBtn

--- GitAddTerminal/ViewController.swift
+++ GitAddTerminal/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .purple
+        setUpBtn()
     }
-
-
+    
+    func setUpBtn() {
+        
+    }
+    
 }
 
